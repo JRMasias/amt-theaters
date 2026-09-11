e362ee69a5a29928eedcceda86722d120af30b9e
jrmasias <johnathonmasias@gmail.com>
2026-09-10 22:46:13 -0500

    Create mobile nav UI

--- components/navigation/mobilemenu.tsx
+++ components/navigation/mobilemenu.tsx
@@ -1,8 +1,32 @@
+import clsx from "clsx";
+import { Menu, XCircle } from "lucide-react";
+import { useState, useEffect } from "react";
+
 export default function MobileMenu()
 {
+     const [ isOpen, setIsOpen ] = useState(false);
+
+     useEffect(() =>
+     {
+     }, []);
+
+     function handleMenuItemClick()
+     {
+          setIsOpen((prev) => !prev);
+     }
+
      return (
           <nav>
-               Mobile Menu
+               <button onClick={ handleMenuItemClick } className="flex flex-col justify-center items-center gap-2 z-10 ">
+                    { !isOpen ? <Menu size={ 30 } color="#ffaa00" /> : <XCircle size={ 30 } color="#ffaa00" /> }
+               </button>
+
+               <ul id="mobile-menu" className={ clsx("absolute left-0 w-full mt-16 bg-neutral-900/30 text-[#ffaa00] font-bold text-2xl shadow-lg flex flex-col items-center gap-2 py-4 transition-all duration-500 ease-in-out", isOpen ? "top-0 " : "-top-100") }>
+                    <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Home</li>
+                    <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Movies</li>
+                    <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Order</li>
+                    <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">Contact</li>
+               </ul>
           </nav>
      );
 }
--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import DesktopMenu from "./desktopmenu";
 import MobileMenu from "./mobilemenu";
+import { Search, Ticket, User2 } from "lucide-react";
 
 export default function Navbar()
 {
@@ -16,8 +18,25 @@ export default function Navbar()
      }, []);
 
      return (
-          <header>
-               { screenWidth > 768 ? <DesktopMenu /> : <MobileMenu /> }
+          <header className="flex justify-between items-center p-3 w-full sticky top-0 z-50 bg-neutral-950">
+               { screenWidth <= 768 &&
+                    <div className="flex items-center justify-between gap-2 text-sm w-full">
+                         <div className="flex items-center">
+                              <MobileMenu />
+                              <Search />
+                         </div>
+                         { screenWidth > 768 ? <></> :
+                              <div className="flex items-center gap-4">
+                                   <Image alt="AMT Logo" src="/images/9.png" width={ 2135 } height={ 736 } className="w-20" />
+                                   <div className="flex items-center">
+                                        <Ticket />
+                                        <p>Showtimes</p>
+                                   </div>
+                                   <User2 className="border-2 rounded-full" />
+                              </div> }
+                    </div>
+               }
+               { screenWidth > 768 && <></> }
           </header>
      );
 }
